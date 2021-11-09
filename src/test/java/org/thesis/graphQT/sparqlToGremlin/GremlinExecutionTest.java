014c0b26fced4504e740313eea8cdceddf410239
emredemircan <emre.demircan@ceng.deu.edu.tr>
2021-11-09 15:35:48 +0100

    Sparql to Gremlin: TınkerGraph execution test is added

---
+++ src/test/java/org/thesis/graphQT/sparqlToGremlin/GremlinExecutionTest.java
@@ -0,0 +1,132 @@
+package org.thesis.graphQT.sparqlToGremlin;
+
+import org.apache.tinkerpop.gremlin.process.traversal.IO;
+import org.apache.tinkerpop.gremlin.process.traversal.Order;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.Scope;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
+import org.apache.tinkerpop.gremlin.structure.Column;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.junit.Before;
+import org.junit.Test;
+import org.thesis.graphQT.gremlin.TinkerGraphCreator;
+
+import java.util.concurrent.Callable;
+
+public class GremlinExecutionTest extends TinkerGraphCreator {
+
+    private final static String BSBM_GRAPHML_1000 = "src/test/resources/graphs/bsbm_scale_1000.graphml";
+
+    private Graph graph;
+    private GraphTraversalSource g;
+
+    @Before
+    public void setUp() {
+        final String selectedGraphML = BSBM_GRAPHML_1000;
+        graph = graph();
+        g = graph.traversal();
+        g.io(selectedGraphML).with(IO.reader, IO.graphml).read().iterate();
+    }
+
+    @Test
+    public void calculateWarmExecutionTimeForQueries() throws Exception {
+        int warmUpRuns = 20;
+
+        //WarmUp
+        for (int i = 0; i < warmUpRuns; i++) {
+            calculateExecutionTimeForAllQueries();
+        }
+        //Actual Run
+        System.out.println("Actual Run");
+        calculateExecutionTimeForAllQueries();
+    }
+
+
+    @Test
+    public void calculateExecutionTimeForAllQueries() throws Exception {
+        Callable<GraphTraversal> callable = () -> g.V().match(__.as("a").values("type").is("review"), __.as("a").out().as("product")).dedup("product").count();
+        calculateExecutionTimeForSingleQuery(1, callable);
+
+        callable = () -> g.V().match(__.as("a").values("ProductPropertyTextual_1").as("property"), __.as("a").values("productID").as("pid"), __.where(__.as("pid").is(P.eq((int) 343)))).select("property");
+        calculateExecutionTimeForSingleQuery(2, callable);
+
+        callable = () -> g.V().match(__.as("a").values("reviewerID").as("rid"), __.as("a").out().as("review"), __.as("review").values("Rating_2").as("r1"), __.where(__.as("rid").is(P.eq((int) 86)))).select("a", "review", "r1");
+        calculateExecutionTimeForSingleQuery(3, callable);
+
+        callable = () -> g.V().match(__.as("a").values("productID").as("pid"), __.as("a").values("ProductPropertyNumeric_1").as("property1"), __.as("a").values("ProductPropertyNumeric_2").as("property2"), __.where(__.and(__.as("property1").is(P.eq((int) 1)), __.as("property2").is(P.gt((int) 100))))).select("pid", "property1", "property2");
+        calculateExecutionTimeForSingleQuery(4, callable);
+
+        callable = () -> g.V().match(__.as("reviewer").values("type").is("reviewer"), __.as("reviewer").values("country").as("country")).select("country").groupCount();
+        calculateExecutionTimeForSingleQuery(5, callable);
+
+        callable = () -> g.V().match(__.as("product").values("ProductPropertyNumeric_4").as("propn4"))
+                .select("propn4").groupCount().order(Scope.local).by(Column.keys, Order.asc)
+                .range(Scope.local, 0L, 20L);
+        calculateExecutionTimeForSingleQuery(6, callable);
+
+        callable = () -> g.V().match(__.as("review").values("type").is("review"), __.as("review").out().as("product"), __.as("product").values("productID").as("pid")).select("pid").groupCount().order(Scope.local).by(Column.values, Order.desc).range(Scope.local, 0L, 10L);
+        calculateExecutionTimeForSingleQuery(7, callable);
+
+        callable = () -> g.V().match(
+                __.as("a").values("type").is("review"),
+                __.as("a").values("Rating_2").as("rating"),
+                __.as("a").out().as("product"),
+                __.as("product").values("productID").as("pid"),
+                __.where(__.as("pid").is(P.eq((int) 343)))
+        ).select("rating").range(0L, 2L);
+        calculateExecutionTimeForSingleQuery(8, callable);
+
+        callable = () -> g.V().match(
+                __.as("reviewer").values("type").is("reviewer"),
+                __.as("reviewer").out().as("review"),
+                __.as("review").out().as("product")
+        ).select("reviewer").groupCount()
+                .order(Scope.local).by(Column.values, Order.desc)
+                .range(Scope.local, 0L, 10L);
+        calculateExecutionTimeForSingleQuery(9, callable);
+
+        callable = () -> g.V().match(
+                __.as("producer").values("type").is("producer"),
+                __.as("producer").values("label_n").as("plabel"),
+                __.as("producer").out().as("product"),
+                __.as("product").values("type").is("product"),
+                __.as("product").values("productID").as("pid"),
+                __.as("product").values("label_n").as("label"),
+                __.as("product").values("comment").as("comment"),
+                __.as("product").values("ProductPropertyTextual_1").as("proptext1"),
+                __.as("product").values("ProductPropertyTextual_2").as("proptext2"),
+                __.as("product").values("ProductPropertyTextual_3").as("proptext3"),
+                __.as("product").values("ProductPropertyNumeric_1").as("propnum1"),
+                __.as("product").values("ProductPropertyNumeric_2").as("propnum2"),
+                __.as("product").out().as("pfeature"),
+                __.as("pfeature").values("type").is("product_feature"),
+                __.as("pfeature").values("label_n").as("flabel"),
+                __.where(__.as("pid").is(P.eq((int) 343)))
+        ).select("plabel", "label", "flabel", "proptext1", "proptext2", "proptext3", "propnum1", "propnum2", "comment")
+                .range(0L, 1L);
+        calculateExecutionTimeForSingleQuery(10, callable);
+
+        callable = () -> g.V().union(__.match(__.as("a").values("productID").as("pid"), __.as("a").values("label_n").as("label"), __.as("a").values("ProductPropertyNumeric_1").as("ppn1"), __.where(__.as("pid").is(P.eq((int) 343)))), __.match(__.as("a").values("productID").as("pid"), __.as("a").values("label_n").as("label"), __.as("a").values("ProductPropertyNumeric_1").as("ppn1"), __.where(__.as("pid").is(P.eq((int) 350))))).select("pid", "label", "ppn1");
+        calculateExecutionTimeForSingleQuery(11, callable);
+    }
+
+    public void calculateExecutionTimeForSingleQuery(int qID, Callable<GraphTraversal> fn) throws Exception {
+        long startTime, endTime;
+        startTime = System.nanoTime();
+        fn.call();
+        endTime = System.nanoTime();
+        final double total = (endTime - startTime) / 1e6;
+        System.out.println(qID + ": " + total);
+        System.out.println("------------------");
+    }
+
+    @Test
+    public void singleQueryTest() {
+        g.V().match(__.as("a").values("reviewerID").as("rid"), __.as("a").out().as("review"),
+                __.as("review").values("Rating_2").as("r1"), __.where(__.as("rid").is(P.eq((int) 86))))
+                .select("a", "review", "r1").toList().forEach(System.out::println);
+
+    }
+}
---
+++ src/test/java/org/thesis/graphQT/sparqlToGremlin/SparqlExecutionTest.java
@@ -0,0 +1,126 @@
+package org.thesis.graphQT.sparqlToGremlin;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.sql.*;
+
+import static org.thesis.graphQT.ResourceHelper.loadOriginalBSBMSparqlQuery;
+
+public class SparqlExecutionTest {
+
+    static String urlDB = "jdbc:virtuoso://localhost:1111";
+    Statement st;
+
+    private final String prefix = "query";
+    private final int totalQuery = 11;
+
+    @Before
+    public void setUp() throws Exception {
+        Class.forName("virtuoso.jdbc4.Driver");
+        Connection conn = DriverManager.getConnection(urlDB, "dba", "dba");
+        st = conn.createStatement();
+    }
+
+    @Test
+    public void name() throws SQLException {
+        String query = "sparql SELECT * from <http://example.com> WHERE {?s ?p ?o}";
+        ResultSet rs = st.executeQuery(query);
+        prnRs(rs);
+    }
+
+    @Test
+    public void calculateExecutionTime() throws Exception {
+        long startTime, endTime;
+        for (int qID = 1; qID < totalQuery + 1; qID++) {
+            //for (int qID = totalQuery; qID > 0; qID--) {
+            final String inputQuery = "sparql " + loadOriginalBSBMSparqlQuery(prefix, qID);
+
+            startTime = System.nanoTime();
+            ResultSet rs = st.executeQuery(inputQuery);
+            countResultSet(rs);
+            endTime = System.nanoTime();
+
+            final double total = (endTime - startTime) / 1e6;
+            System.out.println(qID + ": " + total);
+            System.out.println("------------------");
+        }
+    }
+
+    @Test
+    public void calculateColdExecutionTimeForSingleQuery() throws Exception {
+        int qID = 3;
+        calculateExecutionTime(qID);
+    }
+
+    @Test
+    public void calculateWarmExecutionTimeForQueries() throws Exception {
+        int warmUpRuns = 20;
+
+        //WarmUp
+        for (int i = 0; i < warmUpRuns; i++) {
+            calculateExecutionTimeForAllQueries();
+        }
+        //Actual Run
+        System.out.println("Actual Run");
+        calculateExecutionTimeForAllQueries();
+    }
+
+    private void calculateExecutionTimeForAllQueries() throws IOException, SQLException {
+        for (int qID = 1; qID < totalQuery + 1; qID++) {
+            calculateExecutionTime(qID);
+        }
+    }
+
+    private void calculateExecutionTime(int qID) throws IOException, SQLException {
+        long startTime, endTime;
+        final String inputQuery = "sparql " + loadOriginalBSBMSparqlQuery(prefix, qID);
+
+        startTime = System.nanoTime();
+        ResultSet rs = st.executeQuery(inputQuery);
+        //countResultSet(rs);
+        endTime = System.nanoTime();
+
+        final double total = (endTime - startTime) / 1e6;
+        System.out.println(qID + ": " + total);
+        System.out.println("------------------");
+    }
+
+    private void countResultSet(ResultSet rs) throws SQLException {
+        int i = 0;
+        while (rs.next()) {
+            i++;
+        }
+        System.out.println("Total result size: " + i);
+    }
+
+    void prnRs(ResultSet rs) {
+        try {
+            ResultSetMetaData rsmd;
+
+            System.out.println(">>>>>>>>");
+            rsmd = rs.getMetaData();
+            int cnt = rsmd.getColumnCount();
+
+            while (rs.next()) {
+                Object o;
+
+                System.out.print("Thread:" + Thread.currentThread().getId() + "  ");
+                for (int i = 1; i <= cnt; i++) {
+                    o = rs.getObject(i);
+                    if (rs.wasNull())
+                        System.out.print("<NULL> ");
+                    else
+                        System.out.print("[" + o + "] ");
+                }
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        System.out.println(">>>>>>>>");
+    }
+}
